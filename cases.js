e44be1ba4c2166b9df465e2cd3f8242eb7f39cd7
Anne <coloradoz99@gmail.com>
2018-02-04 16:44:54 -0700

    movies working

--- cases.js
+++ cases.js
@@ -32,17 +32,46 @@ switch (action) {
 // functions 
   function myTweets() {
     if (action="my-tweets") {
-      console.log("you are in the myTweets function")
-  //   inquirer.prompt([
-  //     {
-  //       type: "password",
-  //       mask:'*',
-  //       name: "twitterAccount",
-  //       message: "What is your Twitter."
-  //     }
-  //  ]).then(function(user){
-  //     console.log("lets see your last 20 tweets"+ user.twitterAccount);
-  //   });//this is the end ot the inquier ".then"
+      // console.log("you are in the myTweets function")
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your name?"
+      },
+      {
+        type: "password",
+        mask:'*',
+        name: "twitterAccount",
+        message: "Enter your Twitter password to see tweets: "
+      }
+   ]).then(function(inquirerResponse){
+      //  console.log("yep");
+                  
+            var client = new Twitter({
+              consumer_key: 'ACaLflkZ0Gx8qfcEO2zPb56JK',
+              consumer_secret: '89So669hTLboZhVLxKlXkAfPAz4bJlzGvsosS1hPZWLh6uO3Xs',
+              access_token_key: '959538956932071424-9gxgNgcrxqx0HZSzGVCdvzyPckjMUM3',
+              access_token_secret: '3H3Dm4I11Da1165aMqA1PN0lx3fUm1YMxFIECqc72eoAP'
+            });
+            
+            var params = {screen_name: 'nodejs'};
+            client.get('statuses/user_timeline', {q: 'node.js'}, function(error, tweets, response) {
+              if (!error) {
+              // console.log(tweets);
+              for (var i = 0; i < tweets.length; i++) {
+                      //show the last 20 tweets and when created 
+                  // do something with data 
+                  console.log("____________________________________________________");
+                  console.log(tweets[i].created_at);
+                  console.log(tweets[i].text);
+              }
+                } else{
+                console.log(error);
+                }
+            });
+    });//this is the end ot the inquier ".then"
+
     }
       };
       
