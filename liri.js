098efc6671fc48b60bcd3d23ef35c08691b69914
Anne <coloradoz99@gmail.com>
2018-02-04 17:10:06 -0700

    working liri

--- liri.js
+++ liri.js
@@ -4,156 +4,213 @@ var request = require("request");
 var fs = require("fs");
 // Load the inquirer pacakge
 var inquirer = require("inquirer");
+//this will take the user input for movie untill i get inquire working 
 //twitter
 var Twitter = require('twitter');
+//spotify
+var Spotify = require('node-spotify-api');
+//users choice of my-tweets spotify-this-song  movie-ths do-wht-it-says.
+var action = process.argv[2];
 
-//this will take the user input for movie until i get inquire working 
-
-var action = user.whichAction//from the which action list 
-
-//link to keys  store as a variable 
-// var twitterKeys = //link to file 
-// var spotifyKeys =//link to file 
-inquirer.prompt([
-  {
-    type: "input",
-    name: "name",
-    message: "What is your name?"
-  },
-  {
-    type: "password",
-    mask:'*',
-    name: "twitterAccount",
-    message: "What is your Twitter."
-  },
-  {
-    type: "input",
-    name: "movieName",
-    message: "What movie do you like?"
-  },
-  {
-    type: "input",
-    name: "songName",
-    message: "What song do you like?"
-  },
-  {
-    type: "list",
-    name: "whichAction",
-    message: "What can LIRI do for you today?",
-    choices: ["my-tweets", "spotify-this-song", "movie-this","do-what-it-says"]
-  },
-]).then(function(user){
-//switch cases 
 switch (action) {
-    //my-tweets
-    case "my-tweets":
-      myTweets();
-      break;
-    //spotify-this-song
-    case "spotify-this-song":
-      spotifySong();
-      break;
-    //movie-this
-    case "movie-this":
-     movieThis();
-      break;
-    //do-what-it-says
-    case "do-what-it-says":
-      doWhat();
-      break;
-  }
-});//this is the end ot the inquier ".then"
-// functions 
-function myTweets() {
-  if (user.whichAction ="my-tweets") {
-    console.log("you are in the myTweets function")
-  }
-//1. `node liri.js my-tweets`
-//* This will show your last 20 tweets and when they were created at in your terminal/bash window.
-    }
-function  spotifySong(){    
-  if (user.whichAction ="spotify-this-song") {
-    console.log("you are in the spotifySong function")
-  }
-// 2. `node liri.js spotify-this-song '<song name here>'`
+  case "my-tweets":
+   myTweets();
+    break;
 
-//    * This will show the following information about the song in your terminal/bash window
-     
-//      * Artist(s)
-     
-//      * The song's name
-     
-//      * A preview link of the song from Spotify
-     
-//      * The album that the song is from
+  case "spotify-this-song":
+  spotifySong();
+    break;
 
-//    * If no song is provided then your program will default to "The Sign" by Ace of Base.
-    }
-function  movieThis(){
-  if (user.whichAction ="movie-this") {
-    console.log("you are in the movieThis function")
-  }
-  
-// 3. `node liri.js movie-this '<movie name here>'` for the inquire syntax
-var UserChoiceFromInquire = nodeArgs;//combine this but basicly either the movie name or movie input from another prompt
-// Store all of the arguments in an array
-var nodeArgs = process.argv;// need to use the "inquire to get the input also add to the json and requred above "
-// Create an empty variable for holding the movie name
-var movieName = "";
-// Loop through all the words in the node argument
-// And do a little for-loop magic to handle the inclusion of "+"s
-  for (var i = 2; i < nodeArgs.length; i++) {
-  if (i > 2 && i < nodeArgs.length) {
-    movieName = movieName + "+" + nodeArgs[i];
-    }  else {
-      movieName += nodeArgs[i];
-    }
-  }
-// Then run a request to the OMDB API with the movie specified
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-// This line is just to help us debug against the actual URL.
-console.log(queryUrl);
-request(queryUrl, function(error, response, body) {
-  // If the request is successful
-  if (!error && response.statusCode === 200) {
-    // Parse the body of the site get:
-    //* Title of the movie.
-    console.log("Title: " + JSON.parse(body).Title);
-        // * Year the movie came out.
-    console.log("Release Year: " + JSON.parse(body).Year);
- //        * IMDB Rating of the movie  ``  
-    console.log("IMDB Rating of this movie: " + JSON.parse(body).Rated);
-    //        * Rotten Tomatoes Rating of the movie.
-    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
-    //        * Country where the movie was produced.
-    console.log("Country producing movie: " + JSON.parse(body).Country);
-    //        * Language of the movie.
-    console.log("Language of the movie: " + JSON.parse(body).Language);
-        //        * Plot of the movie.
-    console.log("Actors in the movie: " + JSON.parse(body).Plot);
-    //        * Actors in the movie.
-    console.log("Actors in the movie: " + JSON.parse(body).Actors);
-      }
-    });
+  case "movie-this":
+  movieThis();
+    break;
+
+  case "do-what-it-says":
+    doWhat();
+    break;
 }
-//*********************************************************** */
-function  doWhat(){
-  if (user.whichAction ="do-what-it-says") {
-    console.log("you are in the doWhat function")
-  }
-// 4. `node liri.js do-what-it-says`
-   
-//    * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-     
-//      * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
-     
-//      * Feel free to change the text in that document to test out the feature for other commands.
-  }
 
-// ### BONUS
 
-// * In addition to logging the data to your terminal/bash window, output the data to a .txt file called `log.txt`.
+// functions 
+  function myTweets() {
+    if (action="my-tweets") {
+      // console.log("you are in the myTweets function")
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your name?"
+      },
+      {
+        type: "password",
+        mask:'*',
+        name: "twitterAccount",
+        message: "Enter your Twitter password to see tweets: "
+      }
+   ]).then(function(inquirerResponse){
+      //  console.log("yep");
+                  
+            var client = new Twitter({
+              consumer_key: 'ACaLflkZ0Gx8qfcEO2zPb56JK',
+              consumer_secret: '89So669hTLboZhVLxKlXkAfPAz4bJlzGvsosS1hPZWLh6uO3Xs',
+              access_token_key: '959538956932071424-9gxgNgcrxqx0HZSzGVCdvzyPckjMUM3',
+              access_token_secret: '3H3Dm4I11Da1165aMqA1PN0lx3fUm1YMxFIECqc72eoAP'
+            });
+            
+            var params = {screen_name: 'nodejs'};
+            client.get('statuses/user_timeline', {q: 'node.js'}, function(error, tweets, response) {
+              if (!error) {
+              // console.log(tweets);
+              for (var i = 0; i < tweets.length; i++) {
+                      //show the last 20 tweets and when created 
+                  // do something with data 
+                  console.log("____________________________________________________");
+                  console.log(tweets[i].created_at);
+                  console.log(tweets[i].text);
+              }
+                } else{
+                console.log(error);
+                }
+            });
+    });//this is the end ot the inquier ".then"
 
-// * Make sure you append each command you run to the `log.txt` file. 
+    }
+      };
+      
+  function  spotifySong(){    
+    if (action ="spotify-this-song") {
+      // console.log("you are in the spotify Song function")
+      inquirer.prompt([
+        {
+          type: "input",
+          name: "songname",
+          message: "What song do you like?"
+        },
+     ]).then(function(inquirerResponse){
+      var songRes = inquirerResponse.songname;
+      var songName = "";
+        // console.log("song res "+ songRes);
+      var songName = songRes.split(); 
+      // console.log("song res "+ songName);
+      //build url  
+          var spotify = new Spotify({
+            id: '32bf67364ca74add92adac229ad286a3',
+            secret: 'f7e36e90658b48d4bc74ac7838ba29a9'
+             });
 
-// * Do not overwrite your file each time you run a command.
+          spotify.search({ type: 'track', query: songName, }, function(err, data) {
+              if (err) {
+                return console.log('Error occurred: ' + err);
+                return;
+              }
+              //    console.log(data);
+              //  console.log(data.tracks.items[0]);
+              //artist name
+                  console.log(data.tracks.items[0].artists[0].name); 
+              //song name     
+                  console.log(data.tracks.items[0].name); 
+                //album name 
+                  console.log(data.tracks.items[0].album.name);
+                //external url  
+                  console.log(data.tracks.items[0].external_urls.spotify); 
+                });
+      });//this is the end ot the inquier ".then" 
+    }
+    };
+
+  function  movieThis(){
+    if (action="movie-this") {
+      // console.log("you are in the movieThis function")
+      inquirer.prompt([
+        {
+          type: "input",
+          name: "moviename",
+          message: "What movie do you like?"
+        },
+     ]).then(function(inquirerResponse){
+        var movieRes = inquirerResponse.moviename;
+        var movieName = "";
+        // console.log("The var moviename is:  "+movieRes);
+        var movieName = movieRes.split();
+            // Then run a request to the OMDB API with the movie specified
+            var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+            // This line is just to help us debug against the actual URL.
+            console.log(queryUrl);
+            request(queryUrl, function(error, response, body) {
+              // If the request is successful
+              if (!error && response.statusCode === 200) {
+                // Parse the body of the site and recover just the imdbRating
+                // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+                console.log("Release Year: " + JSON.parse(body).Year);
+                console.log("Release Year: " + JSON.parse(body).Year);
+                console.log("Title: " + JSON.parse(body).Title);
+                console.log("IMDB Rating of this movie: " + JSON.parse(body).Rated);
+                console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
+                console.log("Country producing movie: " + JSON.parse(body).Country);
+                console.log("Language of the movie: " + JSON.parse(body).Language);
+                console.log("Plot " + JSON.parse(body).Plot);
+                console.log("Actors in the movie: " + JSON.parse(body).Actors);
+              }
+            });
+      });//this is the end ot the inquier ".then" 
+    }
+  };
+
+  function  doWhat(){
+    if (action ="do-what-it-says") {
+      console.log("you are in the doWhat function")
+      }
+    };
+
+
+
+
+
+// inquirer.prompt([
+//   {
+//     type: "input",
+//     name: "name",
+//     message: "What is your name?"
+//   },
+//   {
+//     type: "list",
+//     name: "whichAction",
+//     message: "What can LIRI do for you today?",
+//     choices: ["my-tweets", "spotify-this-song", "movie-this","do-what-it-says"]
+//   },
+// ]).then(function(user){
+//switch cases 
+// switch (user) {
+//     //my-tweets
+//     case "my-tweets":
+//       myTweets();
+//       break;
+//     //spotify-this-song
+//     case "spotify-this-song":
+//       spotifySong();
+//       break;
+//     //movie-this
+//     case "movie-this":
+//      movieThis();
+//       break;
+//     //do-what-it-says
+//     case "do-what-it-says":
+//       doWhat();
+//       break;
+// //   }
+// if (user.whichAction = "my-tweets") {
+//   myTweets();
+  
+// } else if (user.whichAction = "spotify-this-song") {
+//   spotifySong()
+  
+// } else if (user.whichAction = "movie-this") {
+//   movieThis()
+  
+// } 
+//  else {
+//     doWhat()
+  
+// } 
+// 
+//});//this is the end ot the inquier ".then"
