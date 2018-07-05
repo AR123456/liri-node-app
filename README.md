f1ccb5de1f5f26c52f68d40e346a0d14cbe335c1
AR123456 <coloradoz99@gmail.com>
2018-07-04 19:07:33 -0600

    updates

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# liri-node-app
-liri-node-app
+# Node JS command line Liri app
+
+# A SIRI like app that takes in command line parameters and returns information back from the Twitter, Spotify and OMDB API's
+
+# Under the hood switch cases, API querys, NPM packages and more!
--- liri.js
+++ liri.js
@@ -1,22 +1,23 @@
 //required
-var request = require("request");
+let request = require("request");
 // Load the fs package to read and write
-var fs = require("fs");
-// Load the inquirer pacakge
-var inquirer = require("inquirer");
+let fs = require("fs");
+// Load the inquirer package
+let inquirer = require("inquirer");
 //twitter
-var Twitter = require("twitter");
+let Twitter = require("twitter");
 //spotify
-var Spotify = require("node-spotify-api");
+let Spotify = require("node-spotify-api");
 //users choice of my-tweets spotify-this-song  movie-ths do-wht-it-says.
-var action = process.argv[2];
- //link to keys
-var client = new Twitter(require("./keys.js").twitterKeys);
-var spotify = new Spotify({
+let action = process.argv[2];
+//link to keys
+let client = new Twitter(require("./keys.js").twitterKeys);
+let spotify = new Spotify({
   id: "32bf67364ca74add92adac229ad286a3",
   secret: "f7e36e90658b48d4bc74ac7838ba29a9"
 });
-inquirer.prompt([
+inquirer
+  .prompt([
     {
       type: "list",
       name: "wantToWhat",
@@ -30,8 +31,7 @@ inquirer.prompt([
     }
   ])
   .then(function(answers) {
-    var action = answers.wantToWhat;
-    // console.log(action);
+    let action = answers.wantToWhat;
     switch (action) {
       case "my-tweets":
         myTweets();
@@ -67,117 +67,161 @@ function myTweets() {
       }
     ])
     .then(function(inquirerResponse) {
-      var params = { screen_name: "nodejs" };
-      client.get("statuses/user_timeline", { count: 20 }, function(error,tweets,response) {
+      let params = { screen_name: "nodejs" };
+      client.get("statuses/user_timeline", { count: 20 }, function(
+        error,
+        tweets,
+        response
+      ) {
         if (!error) {
-                  for (var i = 0; i < tweets.length; i++) {
+          for (let i = 0; i < tweets.length; i++) {
             //show tweets and when created
-            console.log(
-              "____________________________________________________"
-            );
+            console.log("____________________________________________________");
             console.log(tweets[i].created_at);
             console.log(tweets[i].text);
-            fs.appendFile("log.txt","\n" +tweets[i].text + "\n" +tweets[i].created_at + "\n" , "utf8", function(err) {
-              if (err) {
-                return console.log(err);
+            fs.appendFile(
+              "log.txt",
+              "\n" + tweets[i].text + "\n" + tweets[i].created_at + "\n",
+              "utf8",
+              function(err) {
+                if (err) {
+                  return console.log(err);
+                }
               }
-            }); 
+            );
           }
         } else {
           console.log(error);
         }
       });
-    }); 
-  }
+    });
+}
 function spotifySong() {
-     inquirer.prompt([
-        {
-          type: "input",
-          name: "songname",
-          message: "What song do you like?"
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "songname",
+        message: "What song do you like?"
+      }
+    ])
+    .then(function(inquirerResponse) {
+      let songRes = inquirerResponse.songname;
+      var songName = "";
+      var songName = songRes.split();
+      //build url
+      spotify.search({ type: "track", query: songName }, function(err, data) {
+        if (err) {
+          return console.log("Error occurred: " + err);
+          return;
         }
-      ]).then(function(inquirerResponse) { 
-        var songRes = inquirerResponse.songname;
-        var songName = "";
-        var songName = songRes.split();
-        //build url
-        spotify.search({ type: "track", query: songName }, function(err, data) {
-          if (err) {
-            return console.log("Error occurred: " + err);
-            return;
-          }
-          //    console.log(data);
-          //artist name
-          console.log(data.tracks.items[0].artists[0].name);
-          //song name
-          console.log(data.tracks.items[0].name);
-          //album name
-          console.log(data.tracks.items[0].album.name);
-          //external url
-          console.log(data.tracks.items[0].external_urls.spotify);
-          //write to log file 
-          fs.appendFile("log.txt","\n" +data.tracks.items[0].artists[0].name + "\n" +data.tracks.items[0].name + "\n" +data.tracks.items[0].album.name + "\n"  +data.tracks.items[0].external_urls.spotify + "\n" , "utf8", function(err) {
+        //    console.log(data);
+        //artist name
+        console.log(data.tracks.items[0].artists[0].name);
+        //song name
+        console.log(data.tracks.items[0].name);
+        //album name
+        console.log(data.tracks.items[0].album.name);
+        //external url
+        console.log(data.tracks.items[0].external_urls.spotify);
+        //write to log file
+        fs.appendFile(
+          "log.txt",
+          "\n" +
+            data.tracks.items[0].artists[0].name +
+            "\n" +
+            data.tracks.items[0].name +
+            "\n" +
+            data.tracks.items[0].album.name +
+            "\n" +
+            data.tracks.items[0].external_urls.spotify +
+            "\n",
+          "utf8",
+          function(err) {
             if (err) {
               return console.log(err);
             }
-          }); 
-        });
-      }); 
-    }
+          }
+        );
+      });
+    });
+}
 function movieThis() {
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          name: "moviename",
-          message: "What movie do you like?"
-        }
-      ])
-      .then(function(inquirerResponse) {
-         console.log("inquire response :  "+inquirerResponse.moviename);
-        if (inquirerResponse.moviename == "") {
-          console.log("no movie selected")
-          var movieRes = "Mr Nobody";
-        } else {
-          var movieRes = inquirerResponse.moviename;
-        }
-      // Then run a request to the OMDB API 
-        var queryUrl =
-          "http://www.omdbapi.com/?t=" +
-          movieRes +
-          "&y=&plot=short&apikey=trilogy";
-        // console.log(queryUrl);
-        request(queryUrl, function(error, response, body) {
-          //  console.log(body);
-          // If the request is successful
-          if (!error && response.statusCode === 200) {
-            // Parse the body 
-            console.log("Release Year: " + JSON.parse(body).Year);
-            console.log("Title: " + JSON.parse(body).Title);
-            console.log("IMDB Rating of this movie: " + JSON.parse(body).Rated);
-            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-            console.log("Country producing movie: " + JSON.parse(body).Country);
-            console.log("Language of the movie: " + JSON.parse(body).Language);
-            console.log("Plot " + JSON.parse(body).Plot);
-            console.log("Actors in the movie: " + JSON.parse(body).Actors);
-            //write to file
-          fs.appendFile("log.txt",
-            "\n" +"Release Year: " + JSON.parse(body).Year +
-            "\n" +"Title: " + JSON.parse(body).Title +
-            "\n" +"IMDB Rating of this movie: " + JSON.parse(body).Rated + 
-            "\n"  +"Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value+ 
-            "\n"  +"Country producing movie: " + JSON.parse(body).Country + 
-            "\n"  +"Language of the movie: " + JSON.parse(body).Language + 
-            "\n"  +"Plot " + JSON.parse(body).Plot + 
-            "\n"  +"Actors in the movie: " + JSON.parse(body).Actors + 
-            "\n" , "utf8", function(err) {
-            if (err) {
-              return console.log(err);
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "moviename",
+        message: "What movie do you like?"
+      }
+    ])
+    .then(function(inquirerResponse) {
+      //  console.log("inquire response :  "+inquirerResponse.moviename);
+      if (inquirerResponse.moviename == "") {
+        // console.log("no movie selected")
+        var movieRes = "Mr Nobody";
+      } else {
+        var movieRes = inquirerResponse.moviename;
+      }
+      // Then run a request to the OMDB API
+      let queryUrl =
+        "http://www.omdbapi.com/?t=" +
+        movieRes +
+        "&y=&plot=short&apikey=trilogy";
+      // console.log(queryUrl);
+      request(queryUrl, function(error, response, body) {
+        //  console.log(body);
+        // If the request is successful
+        if (!error && response.statusCode === 200) {
+          // Parse the body
+          console.log("Release Year: " + JSON.parse(body).Year);
+          console.log("Title: " + JSON.parse(body).Title);
+          console.log("IMDB Rating of this movie: " + JSON.parse(body).Rated);
+          console.log(
+            "Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value
+          );
+          console.log("Country producing movie: " + JSON.parse(body).Country);
+          console.log("Language of the movie: " + JSON.parse(body).Language);
+          console.log("Plot " + JSON.parse(body).Plot);
+          console.log("Actors in the movie: " + JSON.parse(body).Actors);
+          //write to file
+          fs.appendFile(
+            "log.txt",
+            "\n" +
+              "Release Year: " +
+              JSON.parse(body).Year +
+              "\n" +
+              "Title: " +
+              JSON.parse(body).Title +
+              "\n" +
+              "IMDB Rating of this movie: " +
+              JSON.parse(body).Rated +
+              "\n" +
+              "Rotten Tomatoes Rating: " +
+              JSON.parse(body).Ratings[1].Value +
+              "\n" +
+              "Country producing movie: " +
+              JSON.parse(body).Country +
+              "\n" +
+              "Language of the movie: " +
+              JSON.parse(body).Language +
+              "\n" +
+              "Plot " +
+              JSON.parse(body).Plot +
+              "\n" +
+              "Actors in the movie: " +
+              JSON.parse(body).Actors +
+              "\n",
+            "utf8",
+            function(err) {
+              if (err) {
+                return console.log(err);
+              }
             }
-         }); 
-          }
-        });
-      }); 
+          );
+        }
+      });
+    });
 }
 function doWhat() {
   if ((action = "do-what-it-says")) {
@@ -185,10 +229,13 @@ function doWhat() {
       if (error) {
         return console.log(error);
       }
-     var dataArray = data.split(",");
+      let dataArray = data.split(",");
       //get data from array
       if ((dataArray[0] = "spotify-this-song")) {
-          spotify.search({ type: "track", query: dataArray[1] }, function(err,data) {
+        spotify.search({ type: "track", query: dataArray[1] }, function(
+          err,
+          data
+        ) {
           if (err) {
             return console.log("Error occurred: " + err);
             return;
@@ -201,12 +248,25 @@ function doWhat() {
           console.log(data.tracks.items[0].album.name);
           //external url
           console.log(data.tracks.items[0].external_urls.spotify);
-                    //write to log file 
-                    fs.appendFile("log.txt","\n" +data.tracks.items[0].artists[0].name + "\n" +data.tracks.items[0].name + "\n" +data.tracks.items[0].album.name + "\n"  +data.tracks.items[0].external_urls.spotify + "\n" , "utf8", function(err) {
-                      if (err) {
-                        return console.log(err);
-                      }
-                    }); 
+          //write to log file
+          fs.appendFile(
+            "log.txt",
+            "\n" +
+              data.tracks.items[0].artists[0].name +
+              "\n" +
+              data.tracks.items[0].name +
+              "\n" +
+              data.tracks.items[0].album.name +
+              "\n" +
+              data.tracks.items[0].external_urls.spotify +
+              "\n",
+            "utf8",
+            function(err) {
+              if (err) {
+                return console.log(err);
+              }
+            }
+          );
         });
       }
     });
